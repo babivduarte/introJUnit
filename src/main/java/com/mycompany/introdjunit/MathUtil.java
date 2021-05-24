2c0eec4cc1a74ad862fe596ac022729a45680f88
babivduarte <duartebarbara33@gmail.com>
2021-05-24 11:47:43 -0300

    Adding tests with odd and even values

---
+++ src/main/java/com/mycompany/introdjunit/MathUtil.java
@@ -0,0 +1,16 @@
+package com.mycompany.introdjunit;
+
+/**
+ *
+ * @author barbara
+ */
+public class MathUtil {
+    
+    public static int mdc(int a, int b) {
+        
+        if (b > 0 && a % b == 0) 
+            return b;
+        
+        return -1;
+    }
+}
---
+++ src/test/java/com/mycompany/introdjunit/MathUtilTest.java
@@ -0,0 +1,38 @@
+package com.mycompany.introdjunit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author barbara
+ */
+class MathUtilTest {
+    
+    @Test
+    void testMdcAParP1() {
+        final int a = 6;
+        final int b = 3;
+        final int esperado = b;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcAImparP1(){
+        final int a = 9;
+        final int b = 3;
+        final int esperado = b;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcAZeroP1(){
+        final int a = 0;
+        final int b = 3;
+        final int esperado = b;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+}
